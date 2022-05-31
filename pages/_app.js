230cb3bc8faa1f6ae82f45876afd60ecb705cbd4
thefoxes86 <nicola.volpi86@gmail.com>
2022-05-31 08:59:23 +0200

    To change the photo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.scss'
 
+import LoadingScreen from '../components/LoadingComponent'
+import { useEffect, useState } from 'react'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [])
+  return <>{isLoading ? <LoadingScreen /> : <Component {...pageProps} />}</>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex justify-center items-center w-screen h-screen">
-        <h1>Santiago is coming</h1>
+      <main className="flex justify-center align-middle items-center w-screen h-screen flex-col container_born">
+        <div className="lineTop"></div>
+        <div className="boxText">
+          <h1 className=" text-5xl flex">Ciao sono Santiago,</h1>
+          <p className=" text-md flex mt-3">
+            siccome ho già capito che<br></br> i miei gienitori sono poco
+            affidabili,<br></br> conto su di voi...<br></br>
+          </p>
+          <h1 className=" text-5xl flex mt-10">Come?</h1>
+          <p className=" text-md flex mt-3">Con una piccola donazione.</p>
+          <p className=" text-md flex mt-3">Vedrò poi, giorno dopo giorno,</p>
+          <p className=" text-md flex">di cosa avrò davvero bisogno.</p>
+          <p className=" text-md flex mt-10">
+            Intestatari: Zamberoni Selene, Volpi Nicola
+          </p>
+          <p className="text-md flex">Causale: Regalo di nascita</p>
+          <p className=" text-md flex">IBAN: IT76D0306924888100000012156</p>
+        </div>
+        <div className="lineBottom"></div>
       </main>
 
       <footer></footer>
